c21b1c134544e348feeb003d54e8de86ffe8384e
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-10-01 15:56:34 +1300

    Created Project with all Activites Resolves #1

---
+++ Assignment8/app/src/main/java/com/amarjot8/assignment8/EndScreen.java
@@ -0,0 +1,13 @@
+package com.amarjot8.assignment8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_screen);
+    }
+}
