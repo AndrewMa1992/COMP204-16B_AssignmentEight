076724e99281f3bf26da6f187b03decf652f1c07
Amarjot Parmar <amarjot-parmar@hotmail.co.nz>
2016-10-04 02:42:23 +1300

    Added Play Button, Functioned, Needs Positioning

--- Assignment8/app/src/main/java/com/amarjot8/assignment8/Home.java
+++ Assignment8/app/src/main/java/com/amarjot8/assignment8/Home.java
@@ -3,6 +3,8 @@ package com.amarjot8.assignment8;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -11,6 +13,22 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Storing Play button
+        Button b_Play = (Button) findViewById(R.id.playbutton);
+        //When Play Button is pressed
+        b_Play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Open Game
+                launchGameactivity();
+            }
+        });
+
+    }
+
+
+    private void launchGameactivity()
+    {
         //Launching Gameplay Acitvity
         Intent intent = new Intent(this, Gameplay.class);
         startActivity(intent);
