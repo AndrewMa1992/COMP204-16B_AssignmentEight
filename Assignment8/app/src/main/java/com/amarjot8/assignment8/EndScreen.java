1ef082d0383351ebfcf92458c9f280590bdb523e
Luke Read <lr70@students.waikato.ac.nz>
2016-10-05 00:07:54 +1300

    Score is displayed

--- Assignment8/app/src/main/java/com/amarjot8/assignment8/EndScreen.java
+++ Assignment8/app/src/main/java/com/amarjot8/assignment8/EndScreen.java
@@ -2,9 +2,18 @@ package com.amarjot8.assignment8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EndScreen extends AppCompatActivity {
+    // Placeholder array for high scores, to be written into a file for the beta
+    int[] highScores = {10, 8, 6, 6, 6, 5, 3, 3, 1, 0};
+    int score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,28 @@ public class EndScreen extends AppCompatActivity {
         // Set to fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_end_screen);
+
+        // Score not yet implemented, set to 4 as test value
+        score = 4;
+        // Display the score
+        ((TextView)findViewById(R.id.textView_score)).setText(score + "");
+
+
+
+        Button submitButton = (Button)findViewById(R.id.submitScoreButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Open Game
+                SubmitHighScore();
+            }
+        });
+    }
+
+    // Method to be called when the user presses the score submit button
+    public void SubmitHighScore() {
+        // For now, just show a toast with the entered name
+        Toast toast = Toast.makeText(getApplicationContext(), ((EditText)findViewById(R.id.nameInput)).getText(), Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
